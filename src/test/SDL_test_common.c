84454cb1693a047c1151896fdadb4eb048302c37
Eric Wasylishen <ewasylishen@gmail.com>
2017-09-19 00:02:19 -0700

    Revert "add --allow-highdpi-process-wide flag to SDL_test_common"
    This reverts commit 88d113c76405fe184e258355b1254c11063519fe.

--- src/test/SDL_test_common.c
+++ src/test/SDL_test_common.c
@@ -27,7 +27,7 @@
 #include <stdio.h>
 
 #define VIDEO_USAGE \
-"[--video driver] [--renderer driver] [--gldebug] [--info all|video|modes|render|event] [--log all|error|system|audio|video|render|input] [--display N] [--fullscreen | --fullscreen-desktop | --windows N] [--title title] [--icon icon.bmp] [--center | --position X,Y] [--geometry WxH] [--min-geometry WxH] [--max-geometry WxH] [--logical WxH] [--scale N] [--depth N] [--refresh R] [--vsync] [--noframe] [--resize] [--minimize] [--maximize] [--grab] [--allow-highdpi] [--allow-highdpi-process-wide]"
+"[--video driver] [--renderer driver] [--gldebug] [--info all|video|modes|render|event] [--log all|error|system|audio|video|render|input] [--display N] [--fullscreen | --fullscreen-desktop | --windows N] [--title title] [--icon icon.bmp] [--center | --position X,Y] [--geometry WxH] [--min-geometry WxH] [--max-geometry WxH] [--logical WxH] [--scale N] [--depth N] [--refresh R] [--vsync] [--noframe] [--resize] [--minimize] [--maximize] [--grab] [--allow-highdpi]"
 
 #define AUDIO_USAGE \
 "[--rate N] [--format U8|S8|U16|U16LE|U16BE|S16|S16LE|S16BE] [--channels N] [--samples N]"
@@ -207,10 +207,6 @@ SDLTest_CommonArg(SDLTest_CommonState * state, int index)
         return 1;
     }
     if (SDL_strcasecmp(argv[index], "--allow-highdpi") == 0) {
-        state->window_flags |= SDL_WINDOW_ALLOW_HIGHDPI;
-        return 1;
-    }
-    if (SDL_strcasecmp(argv[index], "--allow-highdpi-process-wide") == 0) {
         SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_ENABLED, "1");
         return 1;
     }
