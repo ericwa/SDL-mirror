d6548e24902ffd66feefd86a23e9268f61627660
Eric Wasylishen <ewasylishen@gmail.com>
2017-10-05 22:35:25 -0700

    move WIN_GetDisplayBounds and WIN_GetDisplayUsableBounds over to new WIN_RectToDPIUnaware

--- src/video/windows/SDL_windowsmodes.c
+++ src/video/windows/SDL_windowsmodes.c
@@ -246,6 +246,7 @@ WIN_GetDisplayBounds(_THIS, SDL_VideoDisplay * display, SDL_Rect * rect)
     const SDL_DisplayData *data = (const SDL_DisplayData *)display->driverdata;
     const SDL_VideoData *vid_data = (const SDL_VideoData *)_this->driverdata;
     MONITORINFO minfo;
+    RECT rectScaled;
     BOOL rc;
     int hdpi, vdpi;
 
@@ -261,10 +262,12 @@ WIN_GetDisplayBounds(_THIS, SDL_VideoDisplay * display, SDL_Rect * rect)
         return SDL_SetError("Couldn't find monitor data");
     }
 
-    rect->x = minfo.rcMonitor.left;
-    rect->y = minfo.rcMonitor.top;
-    rect->w = minfo.rcMonitor.right - minfo.rcMonitor.left;
-    rect->h = minfo.rcMonitor.bottom - minfo.rcMonitor.top;
+    WIN_RectToDPIUnaware(_this, minfo.rcMonitor, &rectScaled);
+
+    rect->x = rectScaled.left;
+    rect->y = rectScaled.top;
+    rect->w = rectScaled.right - rectScaled.left;
+    rect->h = rectScaled.bottom - rectScaled.top;
 
     return 0;
 }
@@ -377,6 +380,7 @@ WIN_GetDisplayUsableBounds(_THIS, SDL_VideoDisplay * display, SDL_Rect * rect)
     BOOL rc;
     LPRECT monitor;
     LPRECT work;
+    RECT workScaled;
     int hdpi, vdpi;
 
     if (WIN_GetDisplayDPIInternal(_this, display, &hdpi, &vdpi) != 0) {
@@ -394,10 +398,12 @@ WIN_GetDisplayUsableBounds(_THIS, SDL_VideoDisplay * display, SDL_Rect * rect)
     monitor = &minfo.rcMonitor;
     work = &minfo.rcWork;
 
-    rect->x = work->left;
-    rect->y = work->top;
-    rect->w = work->right - work->left;
-    rect->h = work->bottom - work->top;
+    WIN_RectToDPIUnaware(_this, *work, &workScaled);
+
+    rect->x = workScaled.left;
+    rect->y = workScaled.top;
+    rect->w = workScaled.right - workScaled.left;
+    rect->h = workScaled.bottom - workScaled.top;
 
     return 0;
 }
